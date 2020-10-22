61b53a5988d6eee2857047c865145799c89d9a0f
Sam Adams <sam.adams@virginwines.co.uk>
2020-10-22 10:16:41 +0100

    Fix test import

--- src/test/java/rexy/ConfigTest.java
+++ src/test/java/rexy/ConfigTest.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ConfigTest {
 	
