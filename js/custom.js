839b513c842cbd5f7329a9218d432160501c19b0
samblake <sbadams@gmail.com>
2021-04-09 08:53:13 +0000

    Deploying to gh-pages from  @ 1fc942aedeae94f8395fa0d448df2949ad6abab6 🚀

--- js/custom.js
+++ js/custom.js
@@ -2,34 +2,6 @@
     "use strict";
 
     /* ==============================================
-    ANIMATION -->
-    =============================================== */
-
-    new WOW({
-        boxClass: 'wow', // default
-        animateClass: 'animated', // default
-        offset: 0, // default
-        mobile: true, // default
-        live: true // default
-    }).init();
-
-    /* ==============================================
-    LIGHTBOX -->
-    =============================================== */
-
-    jQuery('a[data-gal]').each(function() {
-        jQuery(this).attr('rel', jQuery(this).data('gal'));
-    });
-    jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({
-        animationSpeed: 'slow',
-        theme: 'light_square',
-        slideshow: true,
-        overlay_gallery: true,
-        social_tools: false,
-        deeplinking: false
-    });
-
-    /* ==============================================
     SCROLL -->
     =============================================== */
 
@@ -59,23 +31,5 @@
     $('[data-spy="scroll"]').each(function () {
       var $spy = $(this).scrollspy('refresh')
     })
-
-    /* ==============================================
-    VIDEO FIX -->
-    =============================================== */
-
-    $(document).ready(function() {
-        // Target your .container, .wrapper, .post, etc.
-        $(".media").fitVids();
-    });
-
-    /* ==============================================
-    VIDEO FIX -->
-    =============================================== */
-
-    $('.docs-sidebar>nav>li>a').click(function() {
-        $('.docs-sidebar>nav>li').removeClass('active');
-        $(this).parent().addClass('active');
-    });
 
 })(jQuery);
